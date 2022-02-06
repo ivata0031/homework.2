279b6ea97f5050fb993147014dd9d417bf93ab73
ivata0031 <98076286+ivata0031@users.noreply.github.com>
2022-02-06 19:05:46 +0200

    Update homework.java

--- src/main/java/homework/two/homework.java
+++ src/main/java/homework/two/homework.java
@@ -1,22 +1,46 @@
 package homework.two;
 
+import java.until.Scanner;
+
 public class homework {
     public static void main(String[] args) {
-        String testString = "My test string";
-        String _testString = "My name is Iva";
-
-        System.out.println("My name is Iva");
-        System.out.println("My last name is Mladzhova");
+        String name = "Iva";
+        String switchName = ("Your name is <myname> ");
+        System.out.println( switchName.replace("<myname>", name));
 
         //Compare two strings to see if they are the same
-        System.out.println(testString.equals(testString + _testString));
+        Scanner scanner = new Scanner(System.in);
+        
+        System.out.println("Enter first string:");
+        String firstString = scanner.nextLine();
+        
+        System.out.println("Enter second string:");
+        String secondString = scanner.nextLine();
+        
+        if (firstString.equals(secondString)){
+            System.out.println("String are equal");
+        }else{
+            System.out.println("String are not equal");
+            
+            
 
         //How many words are in a string?
-        String [] stringArray = testString.split(" ");
-        System.out.println(testString + " has " + stringArray.length + " word.");
+        System.out.println("Enter some text and I will count the words in it:");
+            String toBeCounted = scanner.nextLine();
+            String [] stringArray = toBeCounted.split(" ");
+            System.out.println("\""+toBeCounted + "\"" + " has " + stringArray.length + "words.");
 
          //Combine two strings into one and write it in the console.
-        System.out.println("My test string: "  + "My name is Iva");
+        
+        String String1 = "My name ";
+        String String2 = "is ";
+        String String3 = "Iva.";
+        System.out.println(String1 + String2 + String3);
+           
+
 
     }
 }
+    
+    
+
